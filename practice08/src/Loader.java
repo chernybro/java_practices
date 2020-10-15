6b8dc6cd35ac43084a0b9ad86e78ff5310596c05
chernybro <68295319+chernybro@users.noreply.github.com>
2020-10-15 11:23:51 +0300

    Calculator added

---
+++ practice08/src/Loader.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Loader {
+    public static void main(String[] args) {
+
+        CalcForm frame = new CalcForm();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}

7de57189c6027a7510f01912a4498033d617da28
chernybro <68295319+chernybro@users.noreply.github.com>
2020-12-17 12:47:25 +0300

    updated loaders with log

--- practice14/src/Loader1.java
+++ practice14/src/Loader1.java
@@ -1,13 +1,23 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Loader1 {
     public static void main(String[] args) {
+        Logger logger = logFile();
+
         Scanner scanner = new Scanner(System.in);
         long size = 0;
-        System.out.println("Введите путь до папки");
+        //System.out.println("Введите путь до папки");
+        logger.log(Level.INFO,"Введите путь до папки");
 
         String path = scanner.nextLine();
+
+        logger.log(Level.INFO,path);
 
         try {
             File file = new File(path);
@@ -27,9 +37,12 @@ public class Loader1 {
                     answer = String.valueOf(size/((long)1024*1024*1024*1024)) + " TBytes";
                 }
 
-                System.out.println("Размер папки " + path + " составляет " + answer);
+                //System.out.println("Размер папки " + path + " составляет " + answer);
+                logger.log(Level.INFO, "Размер папки " + path + " составляет " + answer);
+
             } else {
-                System.out.println("Такого каталога нет");
+                //System.out.println("Такого каталога нет");
+                logger.log(Level.WARNING, "Такого каталога нет");
             }
 
 
@@ -49,6 +62,22 @@ public class Loader1 {
                 length += folderSize(file);
         }
         return length;
+    }
+
+    public static Logger logFile() {
+        Logger logger = Logger.getLogger(Loader1.class.getName());
+        FileHandler fh;
+
+        try {
+            fh = new FileHandler("log\\log.txt");
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+            return logger;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
--- practice14/src/Loader3.java
+++ practice14/src/Loader3.java
@@ -3,22 +3,32 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Loader3 {
     public static void main(String[] args) {
+        Logger logger = logFile();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите копируемый каталог");
+        //System.out.println("Введите копируемый каталог");
+        logger.log(Level.INFO,"Введите копируемый каталог");
         String source = scanner.nextLine();
-
-        System.out.println("Куда вставить?");
+        logger.log(Level.INFO, "source = " + source);
+        //System.out.println("Куда вставить?");
+        logger.log(Level.INFO,"Куда вставить?");
         String destination = scanner.nextLine();
+        logger.log(Level.INFO, "destination = " + destination);
 
         try {
             copyDirectory(new File(source), new File(destination + source.substring(source.lastIndexOf("\\"))));
-            System.out.println("Копирование произошло...");
+            //System.out.println("Копирование произошло...");
+            logger.log(Level.INFO, "Копирование произошло...");
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("что то не так, бро");
+            //
+            logger.log(Level.WARNING, "smth got wrong");
         }
     }
 
@@ -57,6 +67,21 @@ public class Loader3 {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+    public static Logger logFile() {
+        Logger logger = Logger.getLogger(Loader3.class.getName());
+        FileHandler fh;
+
+        try {
+            fh = new FileHandler("log\\log.txt");
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+            return logger;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
